db813877871b78e8767d63321bdaaec11dd8c5de
DashaTyshch <dasha.tyshenko98@gmail.com>
2018-04-10 21:35:00 +0300

    test template

--- lu_pet/urls.py
+++ lu_pet/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lu_pet.views import main
+from django.conf.urls import url, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	url('^$', main)
 ]
