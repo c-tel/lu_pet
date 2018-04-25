486fe2017406019e53c0b0ae584e6b320c6c80a1
Stas Telizhenko <alex@alexcp.com>
2018-04-25 03:47:41 +0300

    post feedback

--- lu_pet/settings.py
+++ lu_pet/settings.py
@@ -128,3 +128,10 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'Rorex_555'
+EMAIL_HOST_USER = 'lupet.dispatch@gmail.com'
+EMAIL_PORT = 587
